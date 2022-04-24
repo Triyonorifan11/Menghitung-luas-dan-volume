2202217f57d8adccba26ea749e5ae70f8fbb68cb
Triyono Rifan <triyonorifan01@gmail.com>
2022-04-24 09:21:05 +0700

    Tugas php menghitung luas dan volume

---
+++ index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Tugas Pertemuan 9</title>
+</head>
+
+<body>
+    <!-- navbar -->
+    <section class="col-sm-12">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container">
+                <div class="container-fluid">
+                    <h5 class="col-sm-12 text-light">Menghitung Luas dan Volume Bangun Ruang</h5>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <!-- content -->
+    <section class="col-sm-12 py-5">
+        <form action="" method="POST">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12 mb-5">
+                        <div class="card">
+                            <div class="card-header bg-primary text-light">
+                                <h5 class="card-title">Luas Persegi</h5>
+                            </div>
+                            <div class="card-body">
+                                <div class="mb-3 row">
+                                    <label for="inputPanjang" class="col-sm-2 col-md-2 col-form-label">Panjang</label>
+                                    <div class="col-sm-10 col-md-10">
+                                        <input type="text" class="form-control" id="inputPanjang">
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="inputLebar" class="col-sm-2 col-md-2 col-form-label">Lebar</label>
+                                    <div class="col-sm-10 col-md-10">
+                                        <input type="text" class="form-control" id="inputLebar">
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-md btn-primary">Hitung</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="col-md-6 col-sm-12">
+                        <div class="card">
+                            <div class="card-header bg-primary text-light">
+                                <h5 class="card-title">Luas Persegi</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </form>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
