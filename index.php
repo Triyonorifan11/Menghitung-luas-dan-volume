a880784adcd2eb683d8eb1441b4c59024d2edc17
Novandi Kevin Pratama <kevinpret27@gmail.com>
2022-04-25 22:40:22 +0700

    Tabung

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
+define("PHI", 3.14);
+
 function input($key){
     if(isset($_POST[$key])){
-        return $_POST[$key];
+        return floatval($_POST[$key]);
     }
     return 0;
 }
@@ -145,32 +147,31 @@ function LimasSegiempatVolume()
 }
 
 
-function tabungVolumeLuasPermukaan()
+function tabungVolume()
 {
-    if(isSubmit("submit-tabungan")){
-        $jari = input("jari");
-        $tinggi = input("tinggi");
-        $hasilVolumeTabung = (3.14 * pow($jari, 2) * $tinggi);
-        $hasilLuasPermukaanTabung = (2 * 3.14 * $jari * ($jari + $tinggi));
-      
+    if(isSubmit("submit-tabung")){
+        $jari = input("rTabung");
+        $tinggi = input("tTabung");
+        $hasilVolumeTabung = (PHI * pow($jari, 2) * $tinggi);
+        return $hasilVolumeTabung;
     }else{
-        $hasilVolumeTabung = 0;
-        $hasilLuasPermukaanTabung = 0;
+        return 0;
     }
 }
 
 
-function capsuleVolumeLuasPermukaan(){
-    if(isSubmit("submit-capsule")){
-        $jari = input("jari");
-        $tinggi = input("tinggi");
-        $hasilVolumeCapsule = (3.14 * pow($jari, 2) * $tinggi);
-        $hasilLuasPermukaanCapsule = (2 * 3.14 * $jari * ($jari + $tinggi));
+function tabungLuasPermukaan(){
+    if(isSubmit("submit-tabung")){
+        $jari = input("rTabung");
+        $tinggi = input("tTabung");
+        $hasilLuasPermukaanTabung = (2 * PHI * $jari * ($jari + $tinggi));
+        return $hasilLuasPermukaanTabung;
     }else{
-        $hasilVolumeCapsule = 0;
-        $hasilLuasPermukaanCapsule = 0;
+        return 0;
     }
 }
+
+
 
 ?>
 
@@ -244,7 +245,7 @@ function capsuleVolumeLuasPermukaan(){
                                             </div>
                                         </div>
                                         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                            <a href="/PemWeb/TugasPertemuan9/" class="btn btn-md btn-danger">Hapus</a>
+                                            <a href="" class="btn btn-md btn-danger">Hapus</a>
                                             <button type="submit" name="submit" value="submit" class="btn btn-md btn-primary">Hitung</button>
                                         </div>
                                     </div>
@@ -307,7 +308,7 @@ function capsuleVolumeLuasPermukaan(){
                                             </div>
                                         </div>
                                         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                            <a href="/PemWeb/TugasPertemuan9/" class="btn btn-md btn-danger">Hapus</a>
+                                            <a href="" class="btn btn-md btn-danger">Hapus</a>
                                             <button type="submit" name="submit-balok" value="submit" class="btn btn-md btn-primary">Hitung</button>
                                         </div>
                                     </div>
@@ -369,7 +370,7 @@ function capsuleVolumeLuasPermukaan(){
                                             </div>
                                         </div>
                                         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                            <a href="/PemWeb/TugasPertemuan9/" class="btn btn-md btn-danger">Hapus</a>
+                                            <a href="" class="btn btn-md btn-danger">Hapus</a>
                                             <button type="submit" name="submit-prisma" value="submit" class="btn btn-md btn-primary">Hitung</button>
                                         </div>
                                     </div>
@@ -437,7 +438,7 @@ function capsuleVolumeLuasPermukaan(){
                                             </div>
                                         </div>
                                         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                            <a href="/PemWeb/TugasPertemuan9/" class="btn btn-md btn-danger">Hapus</a>
+                                            <a href="" class="btn btn-md btn-danger">Hapus</a>
                                             <button type="submit" name="submit-limassegitiga" value="submit" class="btn btn-md btn-primary">Hitung</button>
                                         </div>
                                     </div>
@@ -499,7 +500,7 @@ function capsuleVolumeLuasPermukaan(){
                                                 </div>
                                             </div>
                                             <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                                <a href="/PemWeb/TugasPertemuan9/" class="btn btn-md btn-danger">Hapus</a>
+                                                <a href="" class="btn btn-md btn-danger">Hapus</a>
                                                 <button type="submit" name="submit-limassegiempat" value="submit" class="btn btn-md btn-primary">Hitung</button>
                                             </div>
                                         </div>
@@ -508,7 +509,60 @@ function capsuleVolumeLuasPermukaan(){
                             </div>
                         </form>
                     </section>
-                    
+
+                    <!-- Tabung -->
+                    <section id="tabung" class="col-md-10 mb-5">
+                        <form action="#tabung" method="POST">
+                            <div class="card" style="overflow: hidden;">
+                                <div class="card-header bg-primary text-light d-flex justify-content-center align-items-center">
+                                    <h5 class="card-title">Tabung</h5>
+                                </div>
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-lg-3 d-flex justify-content-center align-items-center">
+                                            <img src="https://d26tpo4cm8sb6k.cloudfront.net/img/cylinder.png" alt="LimasSegiempat" style="height: 130px;">
+                                        </div>
+                                        <div class="col-lg-9">
+                                            <div class="mb-1 row">
+                                                <label for="inputRadiusTabung" class="col-sm-3 col-form-label">Jari-jari</label>
+                                                <div class="col-sm-9">
+                                                    <input type="text" name="rTabung" value="<?=input("rTabung")?>"  class="form-control form-control-sm" id="inputRadiusTabung" placeholder="Input Jari-jari Alas">
+                                                </div>
+                                            </div>
+                                            <div class="mb-1 row">
+                                                <label for="inputTinggiTabung" class="col-sm-3 col-form-label">Tinggi Tabung</label>
+                                                <div class="col-sm-9">
+                                                    <input type="text" name="tTabung" value="<?=input("tTabung")?>" class="form-control form-control-sm" id="inputTinggiTabung" placeholder="Input Tinggi Tabung">
+                                                </div>
+                                            </div>
+                                            <?php if(isSubmit("submit-tabung")) :?>
+                                            <div class="mb-1 row">
+                                                <label class="col-sm-2 col-md-3 col-form-label">Luas</label>
+                                                <div class="col-sm-9 col-md-9 mt-1">
+                                                    <h3>
+                                                        <?=tabungVolume()?>
+                                                    </h3>
+                                                </div>
+                                            </div>
+                                            <div class=" mb-1 row">
+                                                <label class="col-sm-2 col-md-3 col-form-label">Volume</label>
+                                                <div class="col-sm-9 col-md-9 mt-1">
+                                                    <h3>
+                                                        <?=tabungLuasPermukaan()?>
+                                                    </h3>
+                                                </div>
+                                            </div>
+                                            <?php endif;?>
+                                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                                <a href="" class="btn btn-md btn-danger">Hapus</a>
+                                                <button type="submit" name="submit-tabung" value="submit" class="btn btn-md btn-primary">Hitung</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </section>
                 </div>
             </div>
     </section>
