ba136be52815cd8144ebdcd27c72d4a58eb8b466
Novandi Kevin Pratama <kevinpret27@gmail.com>
2022-04-25 23:15:01 +0700

    Split view

--- index.php
+++ index.php
@@ -536,117 +536,15 @@ function kapsulLuasPermukaan(){
                     </section>
 
                     <!-- Tabung -->
-                    <section id="tabung" class="col-md-10 mb-5">
-                        <form action="#tabung" method="POST">
-                            <div class="card" style="overflow: hidden;">
-                                <div class="card-header bg-primary text-light d-flex justify-content-center align-items-center">
-                                    <h5 class="card-title">Tabung</h5>
-                                </div>
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-lg-3 d-flex justify-content-center align-items-center">
-                                            <img src="https://d26tpo4cm8sb6k.cloudfront.net/img/cylinder.png" alt="LimasSegiempat" style="height: 130px;">
-                                        </div>
-                                        <div class="col-lg-9">
-                                            <div class="mb-1 row">
-                                                <label for="inputRadiusTabung" class="col-md-3 col-form-label">Jari-jari</label>
-                                                <div class="col-md-9">
-                                                    <input type="text" name="rTabung" value="<?=input("rTabung")?>"  class="form-control form-control-sm" id="inputRadiusTabung" placeholder="Input Jari-jari Alas">
-                                                </div>
-                                            </div>
-                                            <div class="mb-1 row">
-                                                <label for="inputTinggiTabung" class="col-md-3 col-form-label">Tinggi Tabung</label>
-                                                <div class="col-md-9">
-                                                    <input type="text" name="tTabung" value="<?=input("tTabung")?>" class="form-control form-control-sm" id="inputTinggiTabung" placeholder="Input Tinggi Tabung">
-                                                </div>
-                                            </div>
-                                            <?php if(isSubmit("submit-tabung")) :?>
-                                            <div class="mb-1 row">
-                                                <label class="col-sm-2 col-md-3 col-form-label">Luas</label>
-                                                <div class="col-md-9 col-md-9 mt-1">
-                                                    <h3>
-                                                        <?=tabungVolume()?>
-                                                    </h3>
-                                                </div>
-                                            </div>
-                                            <div class=" mb-1 row">
-                                                <label class="col-sm-2 col-md-3 col-form-label">Volume</label>
-                                                <div class="col-md-9 col-md-9 mt-1">
-                                                    <h3>
-                                                        <?=tabungLuasPermukaan()?>
-                                                    </h3>
-                                                </div>
-                                            </div>
-                                            <?php endif;?>
-                                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                                <?php if(isSubmit("submit-tabung")) :?>
-                                                    <a href="" class="btn btn-md btn-danger">Hapus</a>
-                                                <?php endif;?>
-                                                <button type="submit" name="submit-tabung" value="submit" class="btn btn-md btn-primary">Hitung</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </section>
+                    <?php
+                        require_once("tabung.php");
+                    ?>
 
 
                     <!-- kapsul -->
-                    <section id="kapsul" class="col-md-10 mb-5">
-                        <form action="#kapsul" method="POST">
-                            <div class="card" style="overflow: hidden;">
-                                <div class="card-header bg-primary text-light d-flex justify-content-center align-items-center">
-                                    <h5 class="card-title">Kapsul</h5>
-                                </div>
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col-lg-3 d-flex justify-content-center align-items-center">
-                                            <img src="https://d26tpo4cm8sb6k.cloudfront.net/img/capsule.png" alt="LimasSegiempat" style="height: 130px;">
-                                        </div>
-                                        <div class="col-lg-9">
-                                            <div class="mb-1 row">
-                                                <label for="inputRadiusKapsul" class="col-md-3 col-form-label">Jari-jari</label>
-                                                <div class="col-md-9">
-                                                    <input type="text" name="rKapsul" value="<?=input("rKapsul")?>"  class="form-control form-control-sm" id="inputRadiusKapsul" placeholder="Input Jari-jari Alas">
-                                                </div>
-                                            </div>
-                                            <div class="mb-1 row">
-                                                <label for="inputTinggiKapsul" class="col-md-3 col-form-label">Tinggi Kapsul</label>
-                                                <div class="col-md-9">
-                                                    <input type="text" name="tKapsul" value="<?=input("tKapsul")?>" class="form-control form-control-sm" id="inputTinggiKapsul" placeholder="Input Tinggi Kapsul">
-                                                </div>
-                                            </div>
-                                            <?php if(isSubmit("submit-kapsul")) :?>
-                                            <div class="mb-1 row">
-                                                <label class="col-sm-2 col-md-3 col-form-label">Luas</label>
-                                                <div class="col-md-9 col-md-9 mt-1">
-                                                    <h3>
-                                                        <?=kapsulVolume()?>
-                                                    </h3>
-                                                </div>
-                                            </div>
-                                            <div class=" mb-1 row">
-                                                <label class="col-sm-2 col-md-3 col-form-label">Volume</label>
-                                                <div class="col-md-9 col-md-9 mt-1">
-                                                    <h3>
-                                                        <?=kapsulLuasPermukaan()?>
-                                                    </h3>
-                                                </div>
-                                            </div>
-                                            <?php endif;?>
-                                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                                <?php if(isSubmit("submit-kapsul")) :?>
-                                                    <a href="" class="btn btn-md btn-danger">Hapus</a>
-                                                <?php endif;?>
-                                                <button type="submit" name="submit-kapsul" value="submit" class="btn btn-md btn-primary">Hitung</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </section>
+                    <?php
+                        require_once("kapsul.php");
+                    ?>
                 </div>
             </div>
     </section>
